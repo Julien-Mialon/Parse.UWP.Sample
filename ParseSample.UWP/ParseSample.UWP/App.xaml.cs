fbee95a58fd8061a5fe12dc5f462299f1e5a700b
Julien Mialon <julien.mialon@backelite.com>
2016-01-18 11:37:38 +0100

    import basic solution

---
+++ ParseSample.UWP/ParseSample.UWP/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ParseSample.UWP
+{
+	sealed partial class App
+	{
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+
+			ParseService.Initialize();
+        }
+		
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+	        await ParseService.OnLaunchedAsync(e);
+
+            Frame rootFrame = Window.Current.Content as Frame;
+			if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+				rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    
+                }
+				
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            Window.Current.Activate();
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
---
+++ ParseSample.UWP/ParseSample.UWP/ParseService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Parse;
+
+namespace ParseSample.UWP
+{
+	public static class ParseService
+	{
+		public const string APPLICATION_ID = "<App id>";
+
+		public const string NETCLIENT_KEY = "<.net key>";
+
+		public static void Initialize()
+		{
+			ParseClient.Initialize(APPLICATION_ID, NETCLIENT_KEY);
+		}
+
+		public static async Task OnLaunchedAsync(ILaunchActivatedEventArgs args)
+		{
+			await ParseInstallation.CurrentInstallation.SaveAsync();
+
+			await ParseAnalytics.TrackAppOpenedAsync(args);
+		}
+	}
+}
